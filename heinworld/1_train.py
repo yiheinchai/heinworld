0d75b214de2eac6ad409b96031f603c7480ab3a4
Yi Hein Chai <chaiyihein@gmail.com>
2026-03-05 19:22:12 +0000

    fix: adjust dataset length in PongDataset to prevent index out of range error

--- heinworld/1_train.py
+++ heinworld/1_train.py
@@ -31,7 +31,7 @@ class PongDataset(Dataset):
         self.data = data
 
     def __len__(self):
-        return len(self.data)
+        return len(self.data) - 1
 
     def __getitem__(self, index):
         return self.data[index], self.data[index + 1]
